97bba49fb468ec9002dfa3a1a5fae4cc6b168646
ahmedsolimanpts <ahmedmsoly98@gmail.com>
2022-10-15 15:20:26 +0200

    v2

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from dataclasses import dataclass
 import pandas as pd
 import datetime
 import glob
@@ -6,12 +5,20 @@ import warnings
 warnings.filterwarnings('ignore')
 
 def Read_csv_file(file_path):
-    df = pd.read_csv(file_path)
-    return df
+    try:
+        df = pd.read_csv(file_path)
+        return df
+    except Exception as e:
+        raise Exception(e)
 
 def get_csv_files(folder_name):
     files = glob.glob(folder_name+'/*.csv')
-    return files
+    if len(files) > 0:
+        return files
+    else:
+        raise ValueError('Please Enter Valid  Path for Source Data folder No Files Here')
+
+
 
 def create_data_frame(columns_name,files):
     df   = pd.DataFrame(columns=columns_name)
@@ -19,21 +26,34 @@ def create_data_frame(columns_name,files):
     for i in files:
         if flag == 0 :
             df = Read_csv_file(i)
+            print('File \"'+i +'\" Shape Is:')
+            print(df.shape)
             flag = 1
         else:
             new = Read_csv_file(i)
-            # print('File \"'+i +'\" Shape Is:')
-            # print(new.shape)
-            df = pd.concat([df,new])
+            print('File \"'+i +'\" Shape Is:')
+            print(new.shape)
+            df = pd.merge(df,new,on='Date')
+
     return df
 
 def Create_Target_File(Folder_path,data_frame):
-    data_frame.to_csv(Folder_path)
-    print('Finish Create File')
+    try:
+        data_frame.to_csv(Folder_path)
+        print('Finish Create File')
+    except Exception as e :
+        raise Exception(e)
 
 def Create_Logs(File,Process):
-    with open(File,'a') as file:
-        file.write(str(datetime.datetime.now()) + ',    ' + Process+'\n')
+    if glob.glob('logs.txt'):
+        with open(File,'a') as file:
+            file.write(str(datetime.datetime.now()) + ',    ' + Process+'\n')
+    else:
+        with open(File,'a') as file:
+            file.write('Time '+ ',    '+'Message\n')
+            file.write(str(datetime.datetime.now()) + ',    ' + Process+'\n')
+
+
 
 
 
@@ -46,16 +66,20 @@ if __name__ == "__main__":
     source_folder = 'Source Data'
     Target_Folder = 'Target Data/target.csv'
     log_file      = 'logs.txt' 
-    Create_Logs(log_file,'Start Reading Files Name')
-    csv_files = get_csv_files(source_folder)
-    Create_Logs(log_file,'End Reading Files Name')
-    Create_Logs(log_file,'Start Creating Data Frame')
-    df = create_data_frame(columns,csv_files)
-    Create_Logs(log_file,'End Creating Data Frame')
-    print(df.head())
-    Create_Logs(log_file,'Start Creating Data Target File')
-    Create_Target_File(Target_Folder,df)
-    Create_Logs(log_file,'End Creating Data Target File')
+    try:
+        Create_Logs(log_file,'Start Reading Files Name')
+        csv_files = get_csv_files(source_folder)
+        Create_Logs(log_file,'End Reading Files Name')
+        Create_Logs(log_file,'Start Creating Data Frame')
+        df = create_data_frame(columns,csv_files)
+        print('Rows :',df.shape[0] ,' and Columns :', df.shape[1])
+        Create_Logs(log_file,'End Creating Data Frame')
+        Create_Logs(log_file,'Start Creating Data Target File')
+        Create_Target_File(Target_Folder,df)
+        Create_Logs(log_file,'End Creating Data Target File')
+    except Exception as e:
+        Create_Logs(log_file,'Session Failed on ' + str( datetime.datetime.now()))
+        raise Exception('Session Failed on ' + str(datetime.datetime.now()))
 
 
 
